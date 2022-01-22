c35b161b56807270be51a89086e6bb33c3b73198
Matveeva Elena <85180117+kpokoleha@users.noreply.github.com>
2022-01-22 19:46:34 +0300

    lesson 3.1

--- src/main/java/ru/gb/lesson/ChooseProduct.java
+++ src/main/java/ru/gb/lesson/ChooseProduct.java
@@ -27,7 +27,7 @@ public class ChooseProduct {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         webDriver.get("https://pop-music.ru/");
-        // размер окна
+        //установим размер окна
         webDriver.manage().window().setSize(new Dimension(1300, 720));
 
         //создаем действие - Навести курсор на элемент
