599c95ebcf83c2d86c73a496bc8dc60f6f356ebb
Matveeva Elena <85180117+kpokoleha@users.noreply.github.com>
2022-01-22 18:49:24 +0300

    lesson 3.1

--- src/main/java/ru/gb/lesson/App.java
+++ src/main/java/ru/gb/lesson/App.java
@@ -1,13 +1,55 @@
 package ru.gb.lesson;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+
+        //чтобы съэкономить ресурсы настроим браузер (чтобы не грузились картинки)
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--blink-settings=imagesEnabled=false");
+
+
+        WebDriver webDriver = WebDriverManager.chromedriver().capabilities(chromeOptions).create(); //- заменяет строки ниже
+
+        //неявное ожидание
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        webDriver.get("https://pop-music.ru/");
+        //установим размер окна
+        webDriver.manage().window().setSize(new Dimension(1300, 720));
+
+        //Thread.sleep(5000);
+
+        webDriver.findElement(By.linkText("Войти")).click();
+        By authFormLocator = By.xpath("//form[contains(@name,'system_auth')]");
+        new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(authFormLocator)); // явное ожидание - в этом месте ждем 10 сек
+        WebElement authForm = webDriver.findElement(authFormLocator);
+        authForm.findElement(By.name("USER_LOGIN")).sendKeys("autosupertravel@yandex.ru");
+        authForm.findElement(By.name("USER_PASSWORD")).sendKeys("12345678");
+        authForm.findElement(By.xpath("//button[span[text()='Войти']]")).click();
+
+        webDriver.findElement(By.cssSelector("div.header__user")).click();
+        webDriver.findElement(By.linkText("Выйти")).click();
+        //Thread.sleep(3000);
+        //webDriver.quit();
+
+        new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(By.linkText("Войти")));//проверяем, что есть кнопка "Войти"
+        webDriver.quit();
     }
 }
