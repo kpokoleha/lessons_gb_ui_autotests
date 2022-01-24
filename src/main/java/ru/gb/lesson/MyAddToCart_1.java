a17c640f64256dbaeadaa21757b1374f6c31c2e6
Matveeva Elena <85180117+kpokoleha@users.noreply.github.com>
2022-01-24 19:23:47 +0300

    HW_3 rename

--- src/main/java/ru/gb/lesson/MyAddToCart_1.java
+++ src/main/java/ru/gb/lesson/MyAddToCart_1.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-public class AddToCart_1 {
+public class MyAddToCart_1 {
     public static void main(String[] args) {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--blink-settings=imagesEnabled=false");
